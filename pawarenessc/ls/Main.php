36a1b5ea027d0ecaa1c2d9786ead01be4857e30d
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-09 10:23:31 +0900

    Update Main.php

--- pawarenessc/ls/Main.php
+++ pawarenessc/ls/Main.php
@@ -8,12 +8,53 @@ use pocketmine\event\block\BlockBreakEvent;
 
 use pocketmine\Server;
 use pocketmine\Player;
+use pocketmine\item\Item;
 
 class Main extends pluginBase implements Listener{
 	
 	public function onEnable(){
 		$this->getLogger()->info("=========================");
- 		$this->getLogger()->info("NNM-Ranksを読み込みました");
+ 		$this->getLogger()->info("LuckyStoneを読み込みました");
+ 		$this->getLogger()->info("制作者: PawarenessC");
+		$this->getLogger()->info("ライセンス: NYSL Version 0.9982");
+		$this->getLogger()->info("http://www.kmonos.net/nysl/");
+ 		$this->getLogger()->info("バージョン:{$this->getDescription()->getVersion()}");
+ 		$this->getLogger()->info("=========================");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onDisable(){
+		$this->getLogger()->info("=========================");
+ 		$this->getLogger()->info("LuckyStoneを停止しました");
  		$this->getLogger()->info("制作者: PawarenessC");
  		$this->getLogger()->info("バージョン:{$this->getDescription()->getVersion()}");
  		$this->getLogger()->info("=========================");
+	}
+	
+	/**
+ 	* @ignoreCancelled
+ 	*/
+	public function onBreak(BlockBreakEvent $event){
+		$player = $event->getPlayer();
+		$block = $event->getBlock();
+		$id = $block->getId();
+		$x = $block->x;
+		$y = $block->y;
+		$z = $block->z;
+		$pos = new Vector3($x, $y, $z);
+		if($block == 1){
+			if(mt_rand(1,1000) == 1){
+				$item = Item::get(265, 0, mt_rand(1,10)); //鉄
+				$level->dropItem($pos, $item);
+			}
+			
+			if(mt_rand(1,10000) == 1){
+				$item = Item::get(264, 0, mt_rand(1,10)); //ダイヤ
+				$level->dropItem($pos, $item);
+			}
+		}
+	}
+			
+			
+			
+	}
