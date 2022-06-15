31724beddfa62596dad75a6d3371479659c3a3fb
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2022-06-16 00:12:21 +0900

    API4の書き方でやったわよーん
    色々かえたわよ

--- src/pawarenessc/ls/Main.php
+++ src/pawarenessc/ls/Main.php
@@ -4,23 +4,21 @@ namespace pawarenessc\ls;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\event\block\BlockBreakEvent;
 
-use pocketmine\Server;
-use pocketmine\Player;
 use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
 
+use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\math\Vector3;
 
 use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat;
 
 class Main extends pluginBase implements Listener{
 	
 	/* @var Config */
 	public $config;
 	
-	public function onEnable(){
+	public function onEnable(): void {
 		$this->getLogger()->info("=========================");
  		$this->getLogger()->info("LuckyStoneを読み込みました");
  		$this->getLogger()->info("制作者: PawarenessC");
@@ -31,15 +29,15 @@ class Main extends pluginBase implements Listener{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		
 		$this->config = new Config($this->getDataFolder()."Setup.yml", Config::YAML,
-				[
-					"iron_rand_max" => 100,
-					"number_of_iron_max" => 10,
-					"diamond_rand_max" => 1000,
-					"number_of_diamond_max" => 10,
-				]);
+            [
+                "iron_rand_max" => 100,
+                "number_of_iron_max" => 10,
+                "diamond_rand_max" => 1000,
+                "number_of_diamond_max" => 10,
+            ]);
 	}
 	
-	public function onDisable(){
+	public function onDisable(): void{
 		$this->getLogger()->info("=========================");
  		$this->getLogger()->info("LuckyStoneを停止しました");
  		$this->getLogger()->info("制作者: PawarenessC");
@@ -54,11 +52,15 @@ class Main extends pluginBase implements Listener{
 		$player = $event->getPlayer();
 		$block = $event->getBlock();
 		$id = $block->getId();
-		$x = $block->x;
-		$y = $block->y;
-		$z = $block->z;
+        //$x = $block->x;
+		//$y = $block->y;
+	    //$z = $block->z;
+        $x = $block->getPosition()->getX();
+        $y = $block->getPosition()->getY();
+        $z = $block->getPosition()->getZ();
 		$pos = new Vector3($x, $y, $z);
-		$level = $player->getLevel();
+		$level = $player->getWorld();
+        //$level = $player->getLevel();
 		if($id == 1){
 			$config = $this->config->getAll();
 			$iron_rand_max =         $config["iron_rand_max"];
@@ -67,17 +69,14 @@ class Main extends pluginBase implements Listener{
 			$number_of_diamond_max = $config["number_of_diamond_max"];
 			
 			if(mt_rand(1,$iron_rand_max) == 1){
-				$item = Item::get(265, 0, mt_rand(1, $number_of_iron_max)); //鉄
+                $item = ItemFactory::getInstance()->get(265, 0, mt_rand(1, $number_of_iron_max));
 				$level->dropItem($pos, $item);
 			}
 			
 			if(mt_rand(1, $diamond_rand_max) == 1){
-				$item = Item::get(264, 0, mt_rand(1, $number_of_diamond_max)); //ダイヤ
+				$item = ItemFactory::getInstance()->get(264, 0, mt_rand(1, $number_of_diamond_max)); //ダイヤ
 				$level->dropItem($pos, $item);
 			}
 		}
 	}
-			
-			
-			
-	}
+}
